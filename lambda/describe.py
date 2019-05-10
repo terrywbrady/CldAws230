efccd8c6fc3a3ac5644128567a1b8338099a5473
Terry Brady <terrywbrady@gmail.com>
2019-05-10 03:53:46 +0000

    describe inst

--- lambda/describe.py
+++ lambda/describe.py
@@ -1,18 +1,32 @@
 import boto3
 region = 'us-west-2'
 ec2 = boto3.client('ec2', region_name=region)
-reservations = ec2.describe_instances()
+reservations = ec2.describe_instances(
+    Filters=[
+        {
+            'Name': 'tag:DSpace',
+            'Values': ['DSpace']
+        },
+    ]
+)
+fres = []
+
 for res in reservations['Reservations']:
     for instance in res['Instances']:
-        id = instance['InstanceId']
-        name = ""
-        dns = instance['PublicDnsName'] if 'PublicDnsName' in instance else ""
-        if 'Tags' in instance:
-            tags = instance['Tags']
-            for tag in tags:
-                if tag['Key'] == 'Name':
-                    name = tag['Value']
-        print id + "\t" + name + "\t" + dns
+        tags = instance['Tags']
+        obj = { 
+            'found': 1,
+            'name': tags['Name'] if 'Name' in tags else "",
+            'id': instance['InstanceId'],
+            'state': instance['State']['Name'],
+            'dns': instance['PublicDnsName'] if 'PublicDnsName' in instance else ""
+        }
+        fres.append(obj)
+
         for key in instance.keys():
             print "\t- " + key
+
+for f in fres:
+    print str(f)
+    
 print "--- describe done"

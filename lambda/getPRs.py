0cdfe7556565d4e7d85c5cdfcb2185a114e97ea5
Terry Brady <terrywbrady@gmail.com>
2019-05-12 23:09:40 +0000

    get PR's

--- lambda/getPRs.py
+++ lambda/getPRs.py
@@ -7,3 +7,12 @@
 #- should branch be inferred from pull?
 
 #https://api.github.com/repos/DSpace/DSpace/pulls
+
+import boto3
+import urllib2
+import json
+req = urllib2.Request('https://api.github.com/repos/DSpace/DSpace/pulls')
+req.add_header('accept', 'application/json')
+response = urllib2.urlopen(req)
+jsondata = json.load(response)
+print str(jsondata)

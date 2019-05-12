c86988d6244aa5339e29395250a1be480f20153c
Terry Brady <terrywbrady@gmail.com>
2019-05-12 23:25:19 +0000

    github api

--- lambda/getPRs.py
+++ lambda/getPRs.py
@@ -11,8 +11,30 @@
 import boto3
 import urllib2
 import json
-req = urllib2.Request('https://api.github.com/repos/DSpace/DSpace/pulls')
-req.add_header('accept', 'application/json')
-response = urllib2.urlopen(req)
-jsondata = json.load(response)
-print str(jsondata)
+
+def getPRs():
+    req = urllib2.Request('https://api.github.com/repos/DSpace/DSpace/pulls?base=dspace-6_x')
+    req.add_header('accept', 'application/json')
+    response = urllib2.urlopen(req)
+    jsondata = json.load(response)
+    
+    prs = []
+    for pr in jsondata:
+        prs.append({
+            'url': pr['url'],
+            'state': pr['state'],
+            'title': pr['title'],
+            'base': pr['base']['ref'],
+        })
+    return prs
+
+def lambda_handler(event, context):
+    prs = getPRs()
+    return {
+        'statusCode': 200,
+        'body': json.dumps(prs)
+    }
+
+prs=getPRs()
+for pr in getPRs():
+    print pr['url']+" "+pr['base']
